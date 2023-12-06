57c2be8394b8d6c076e236afaf0b1aaef6e2eb83
Luna <69774917+Lluna4@users.noreply.github.com>
2023-12-06 19:54:55 +0100

    Add files via upload

--- ft_print_memory.c
+++ ft_print_memory.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <ctype.h>
 
 int	ft_putchar(char c)
 {
@@ -8,7 +7,17 @@ int	ft_putchar(char c)
 	return (1);
 }
 
-int	ft_hexlen(unsigned long long int n)
+int	ft_isprint(int a)
+
+{
+	if (a >= 32 && a <= 126)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_hexlen(unsigned short n)
 {
 	int	ret;
 
@@ -21,26 +30,41 @@ int	ft_hexlen(unsigned long long int n)
 	return (ret);
 }
 
-int	ft_puthex(unsigned long long n, char may, int iter)
+int	ft_puthex(unsigned int n)
 {
 	int	ret;
 	int	sum;
 
-	if (may == 'X')
-		sum = 55;
-	else
-		sum = 87;
 	ret = ft_hexlen(n);
 	if (n >= 16)
 	{
-        if (ft_puthex(n / 16, may, iter++) == -1)
+        if (ft_puthex(n / 16) == -1)
             return (-1);
 		n = n % 16;
 	}
-	if (n == 0 && iter == 0)
-		return (ft_putchar('0'));
 	if (n > 9)
-		if (ft_putchar(n + sum) == -1)
+		if (ft_putchar(n + 87) == -1)
+			return (-1);
+	if (n <= 9)
+		if (ft_putchar(n + '0') == -1)
+			return (-1);
+	return (ret);
+}
+
+int ft_put_pointer(unsigned long n)
+{
+	int	ret;
+	int	sum;
+
+	ret = ft_hexlen(n);
+	if (n >= 16)
+	{
+        if (ft_put_pointer(n / 16) == -1)
+            return (-1);
+		n = n % 16;
+	}
+	if (n > 9)
+		if (ft_putchar(n + 87) == -1)
 			return (-1);
 	if (n <= 9)
 		if (ft_putchar(n + '0') == -1)
@@ -60,16 +84,22 @@ void print_bytes(void *ptr, int size)
         for (i=total; i<max; i += 2) 
         {
             if (i % 16 == 0)
-               ft_puthex((unsigned long long)&p[i], 'a', 0);
-            ft_puthex((unsigned long long)p[i], 'a', 0);
-            ft_puthex((unsigned long long)p[i + 1], 'a', 0);
+			{
+               ft_put_pointer((unsigned long)&p[i]);
+			   	ft_putchar(':');
+				ft_putchar(' ');
+			}
+            if (ft_puthex((unsigned int)p[i]) < 2)
+				ft_putchar('0');
+            if (ft_puthex((unsigned int)p[i + 1]) < 2)
+				ft_putchar('0');
             ft_putchar(' ');
             //printf("%02hhX%02hhX ",p[i], p[i+1]);
             total += 2;
         }
         for (i=total - 16; i<max; i++) 
         {
-            if (isalnum(p[i]) == 0)
+            if (ft_isprint(p[i]) == 0)
                 ft_putchar('.');
             else
                 ft_putchar(p[i]);
@@ -78,9 +108,9 @@ void print_bytes(void *ptr, int size)
         max += 16;
     }
 }
-
+ 
 int main()
 {
-    char *a = "askjakkkkkkkk";
-    print_bytes(a, 16);
+    char *a = "This is very cool\n I'm having real fun here";
+    print_bytes(a, 10);
 }
